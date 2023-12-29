3ee04830b00cd6eef70fa9bcedc495422a8b9ca9
Uncledave21 <155086391+Uncledave21@users.noreply.github.com>
2023-12-29 22:34:39 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,25 +2,25 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber =  "919931122319"
-global.ownername = "Ayush Pandey"
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber =  "+234 814 372 8690"
+global.ownername = "Raymond"
+global.ytname = "YT: RAYMOND.inc"
+global.socialm = "GitHub: RAYMOND 123"
+global.location = "Nigerian"
 
-global.ownername = 'Ayush' //owner name
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.ownername = 'Raymond' //owner name
+global.botname = 'Ray-bot' //name of the bot
 
 //sticker details
 global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'Ray-bot'
 //console view/theme
 global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.wm = "Raymond inc."
 
 //theme link
-global.link = 'https://chat.whatsapp.com/Jllsl2OaQNoBjepxzuVsZM'
+global.link = 'https://chat.whatsapp.com/GhUtAGIXNM52ebgjDZJ0nq'
 
 //custom prefix
 global.prefa = ['.']
