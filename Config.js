b071712e34956c669d4ff720e37b2b84f3182047
Avitus77 <156421293+Avitus77@users.noreply.github.com>
2024-01-12 19:56:06 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,15 +1,16 @@
+
 const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber =  "919804160882"
+global.ownernumber =  "+255749419546"
 global.ownername = "BlackHeart🖤"
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
 
-global.ownername = 'BlackHeart🖤' //owner name
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.ownername = 'dondeus' //owner name
+global.botname = 'adl' //name of the bot
 
 //sticker details
 global.stickername = '?𓆩‎✘BlackHeart🖤✘𓆪_✰'
@@ -34,28 +35,21 @@ global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti212 = true //auto block +212
 global.autoread_status = false //auto view status/story
+global.auto detect-sticker=true//auto detect sticker
+global.auto answer questions=true//auto detect questions
 
 
 
-//reply messages
-global.mess = {
-    done: '*here you go!* \n\n*🎀 Maria Bot 🎀*\n\n*🧩 Bot link:* \nhttps://github.com/AYUSH-PANDEY023/Maria-MD\n',
-    prem: '*This feature can be used by premium user only*',
-    admin: '*This feature can be used by admin only*',
-    botAdmin: '*This feature can only be used when the bot is a group admin* ',
-    owner: '*This feature can be used by owner only*',
-    group: '*This feature is only for groups*',
-    private: '*This feature is only for private chats*',
-    wait: '*In process...* ',    
-    error: '*Error!*',
-}
 
-global.thumb = fs.readFileSync('./Gallery/thumb.jpg')
 
-let file = require.resolve(__filename)
-fs.watchFile(file, () => {
-    fs.unwatchFile(file)
-    console.log(chalk.redBright(`Update'${__filename}'`))
-    delete require.cache[file]
-    require(file)
-})
+
+
+
+
+
+
+
+
+
+
+
