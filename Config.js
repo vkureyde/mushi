5162784ddb88a4baf720a2a5007b698e957d5717
jerankostin <142435864+jerankostin@users.noreply.github.com>
2024-01-19 23:26:37 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,21 +2,21 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber =  "919931122319"
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber =  "256781604763"
+global.ownername = "techgang"//owner name
+global.ytname = "YT: Techgang.inc"
+global.socialm = "GitHub: Techgang023"
+global.location = "Dreamland"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'TECHGANG' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = 'techgang'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'Techgang Bot'
 //console view/theme
 global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.wm = "Techgang inc."
 
 //theme link
 global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
