8e7b1bbf91b0979443f1e2206ce388278df19a9b
Sandip <155181268+Sandippaul00@users.noreply.github.com>
2024-01-21 13:15:23 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['14703105589']
+global.ownername = Professort//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
