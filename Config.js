d0e85320c8fb81eb88c581798cddf65127674cea
Rayhan1616 <157794442+Rayhan1616@users.noreply.github.com>
2024-01-26 11:44:01 +0200

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,24 +2,24 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
+global.ownernumber = ['260770870221']
+global.ownername = "レイハン"//owner name
+global.ytname = "YT: mk_rayhan"
+global.socialm = "GitHub: Rayhan1616"
 global.location = "Indian"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'RAYHAN-BOTS' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = 'RAYHAN-BOTS'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'RAYHAN Bot'
 //console view/theme
 global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.wm = "RAYHAN botz inc."
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
+global.link = 'https://chat.whatsapp.com/GrXAZMxE76tLIvDGJnDIdy'
 
 //custom prefix
 global.prefa = ['!']
