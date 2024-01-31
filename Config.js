92ee10d7e167dea31b4d1e1e268f0aa9f51ed7b7
Ruvindu123 <158305328+Ruvindu123@users.noreply.github.com>
2024-01-31 22:01:03 +0530

    Config.js

--- Config.js
+++ Config.js
@@ -2,24 +2,24 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = ['720501770']
+global.ownername = "💙𓆩‎✘ruvindu𓆪_✰"//owner name
+global.ytname = "ruvi_music"
+global.socialm = "GitHub: ruvi_editz"
+global.location = "Sri lanka"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'CYBER-RUVI-BOT' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = '💙𓆩‎✘ruvindu𓆪_✰'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'ruvi Bot'
 //console view/theme
 global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.wm = "Ruvi Bot"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
+global.link = 'https://chat.whatsapp.com/IQdsO1X8Rx13AVWKjHWlSC'
 
 //custom prefix
 global.prefa = ['!']
