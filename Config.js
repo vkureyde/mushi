ff3d9d5e9237ddec0d2e3283f0f49d426080f9fa
JRoluosangEditz <159141071+JRoluosangEditz@users.noreply.github.com>
2024-02-06 17:55:05 +0530

    Update Config.js
    Let's go buddy

--- Config.js
+++ Config.js
@@ -2,21 +2,21 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
+global.ownernumber = ['7085345102']
+global.ownername = "💙Sanga Pudaite"//owner name
+global.ytname = "YT: J Rohluosang Editz"
+global.socialm = "GitHub: J Rohluosang Editz"
 global.location = "Indian"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'Sanga bot' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
-global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.stickername = sanga pudaite'
+global.packname = 'Sticker By sanga pudaite bot'
+global.author = 'sanga Bot'
 //console view/theme
 global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.wm = "sanga botz inc."
 
 //theme link
 global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
@@ -38,7 +38,7 @@ global.autoread_status = false //auto view status/story
 
 //reply messages
 global.mess = {
-    done: '*here you go!* \n\n*🎀 Maria Bot 🎀*\n\n*🧩 Bot link:* \nhttps://github.com/AYUSH-PANDEY023/Maria-MD\n',
+    done: '*here you go!* \n\n*🎀 sanga Bot 🎀*\n\n*🧩 Bot link:* \🇪 🇷 🇪 🇳 ',
     prem: '*This feature can be used by premium user only*',
     admin: '*This feature can be used by admin only*',
     botAdmin: '*This feature can only be used when the bot is a group admin* ',
