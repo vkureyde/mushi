c69e9834f86a53f77565154de81a4860340fe95a
Sheriff458f5 <159548922+Sheriff458f5@users.noreply.github.com>
2024-02-10 01:08:35 +0300

    Revert "Sheriff Update Config.js"

--- Config.js
+++ Config.js
@@ -2,11 +2,11 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['0741206672']
+global.ownernumber = ['919931122319']
 global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Kenya"
+global.location = "Indian"
 
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
@@ -25,14 +25,14 @@ global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
 global.prefa = ['!']
 
 //false=disable and true=enable
-global.welcome = true //auto welcome
-global.autoRecording = true //auto recording
-global.autoTyping = true //auto typing
-global.autorecordtype = true //auto typing + recording
-global.autoread = true //auto read messages
-global.autobio = tru8 //auto update bio
+global.welcome = false //auto welcome
+global.autoRecording = false //auto recording
+global.autoTyping = false //auto typing
+global.autorecordtype = false //auto typing + recording
+global.autoread = false //auto read messages
+global.autobio = false //auto update bio
 global.anti212 = true //auto block +212
-global.autoread_status = true //auto view status/story
+global.autoread_status = false //auto view status/story
 
 
 
