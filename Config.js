676da6e8b3624f30246062281a0a5d6e044b1d9c
negus87 <67227345+negus87@users.noreply.github.com>
2024-02-11 14:49:28 +0000

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['07507241123']
+global.ownername = "💙𓆩‎✘IL NEGUS_87  ✘𓆪_✰"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "uk"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'il re dei fattoni' //name of the bot
 
 //sticker details
 global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
