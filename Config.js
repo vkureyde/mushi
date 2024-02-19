faf2a8700c0fb88a8ae4b11a5fd79a9e64530315
Marxcellino02 <160545460+Marxcellino02@users.noreply.github.com>
2024-02-19 23:27:55 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,11 +2,11 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['2348149272182']
+global.ownername = "💙Marcel"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Nigeria"
 
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
