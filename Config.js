44855736bb2d56e821994e5b6a95e0927db7c801
choice2bizzy <132263103+choice2bizzy@users.noreply.github.com>
2024-02-25 14:05:16 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,18 +2,18 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
+global.ownernumber = ['+255693608625']
 global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = '2bizzy' //name of the bot
 
 //sticker details
 global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = '2bizzy'
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
