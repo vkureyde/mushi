170d4f0b8f127593807045984b4a740794cea69b
Mrboda <160818428+Mrboda@users.noreply.github.com>
2024-02-28 11:40:06 +0500

    Revert "Update Config.js"

--- Config.js
+++ Config.js
@@ -11,12 +11,12 @@ global.location = "Indian"
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
 //sticker details
-global.stickername = '💙MR_BODA✰'
+global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
 global.packname = 'Sticker By'
-global.author = 'BODA Bot'
+global.author = 'Maria Bot'
 //console view/theme
 global.themeemoji = '🧩'
-global.wm = "boda botz inc."
+global.wm = "Ayush botz inc."
 
 //theme link
 global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
@@ -38,7 +38,7 @@ global.autoread_status = false //auto view status/story
 
 //reply messages
 global.mess = {
-    done: '*here you go!* \n\n*🎀 Boda Bot 🎀*\n\n*🧩 Bot link:* \nhttps://github.com/Mrboda/Maria-MD\n',
+    done: '*here you go!* \n\n*🎀 Maria Bot 🎀*\n\n*🧩 Bot link:* \nhttps://github.com/AYUSH-PANDEY023/Maria-MD\n',
     prem: '*This feature can be used by premium user only*',
     admin: '*This feature can be used by admin only*',
     botAdmin: '*This feature can only be used when the bot is a group admin* ',
