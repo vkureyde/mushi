5427c8bf49256bd7a25c12640d89f87b83c8d42b
Arizona092 <143114448+Arizona092@users.noreply.github.com>
2024-03-06 06:25:49 -0800

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,18 +2,16 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = ['+2349154766535']
+global.ownername = "💙Arizona"//owner name
+global.location = "Abj"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'KIRITO-KT' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = 'Not your bot'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'Kirito Bot'
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
