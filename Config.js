a4f0e34421b36d9555dc8b6e4a3a27d46899d27a
KobbyWelt <127047095+KobbyWelt@users.noreply.github.com>
2024-03-11 01:05:44 +0000

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,17 +3,17 @@ const chalk = require('chalk')
 
 //contact details
 global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownername = "Kobby"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Ghanaian"
 
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = 'Kobby Welt'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'Kobby Welt'
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
