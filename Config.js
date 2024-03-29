d099f19e545941c6efddb41630a2a546be302e48
Magnum328 <165403386+Magnum328@users.noreply.github.com>
2024-03-29 16:16:59 +0000

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['233592474248']
+global.ownername = "ALSWELL OTABIL"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Ghana"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = '𝐌ÅGNUM-NÉSSÃ' //name of the bot
 
 //sticker details
 global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
