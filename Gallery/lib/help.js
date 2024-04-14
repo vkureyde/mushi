017816d787f928c4d8e40dad1ea8331e40799a02
asmakev930h <142667021+asmakev930h@users.noreply.github.com>
2024-04-14 10:42:24 +0100

    Update help.js

--- Gallery/lib/help.js
+++ Gallery/lib/help.js
@@ -2,13 +2,13 @@ const prefix = global.prefa
   const pushname = m.pushName || "No Name"
 
 const helpmenu=` 
-「 *Maria BOT* 」
+「 *Cobra-MD* 」
 ♡ۣۜۜ፝͜͜͡͡✿➤ *INFO BOT*
-  ♡ۣۜۜ፝͜͜͡͡✿➤ Name: Maria BOT
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Name: Cobra BOT
   ♡ۣۜۜ፝͜͜͡͡✿➤ Version: 1.0
   ♡ۣۜۜ፝͜͜͡͡✿➤ Type: VPS
-  ♡ۣۜۜ፝͜͜͡͡✿➤ Owner: 𝑨𝒚𝒖𝒔𝒉-𝒑𝒂𝒏𝒅𝒆𝒚
-  ♡ۣۜۜ፝͜͜͡͡✿➤ wa.me/919931122319
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Owner: King-Cobra
+  ♡ۣۜۜ፝͜͜͡͡✿➤ wa.me/2347041039367
   ♡ۣۜۜ፝͜͜͡͡✿➤ Prefix: 「  ${prefix}  」
  ♡ۣۜۜ፝͜͜͡͡✿➤ Total cmd: 51
  ♡ۣۜۜ፝͜͜͡͡✿➤Menu Type: 𝑯𝒆𝒍𝒑-𝑴𝒆𝒏𝒖
@@ -79,9 +79,9 @@ const helpmenu=`
 
 🌹𝒎𝒐𝒓𝒆 𝒄𝒐𝒎𝒎𝒂𝒏𝒅𝒔 𝒄𝒐𝒎𝒊𝒏𝒈 𝒔𝒐𝒐𝒏
 
-『 Maria-Md 』
+『 cobra-MD 』
    Based on: Miku, 
-   created by : Ayush pandey
-   ©️ Ayush botz inc 
+   created by : king cobra
+   ©️ cobra botz inc 
 `
 module.exports = helpmenu
