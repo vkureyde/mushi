cb9da764eb7f3cb5e296341638faeddaf17194e9
officailpius <148732471+officailpius@users.noreply.github.com>
2024-05-03 13:11:23 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,8 +3,8 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "2348134641349"],
+global.ownername = process.env.OWNER_NAME || "💙𓆩‎Eliminate𓆪_✰",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
